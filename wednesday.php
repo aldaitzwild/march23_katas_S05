f6d688d928313e3a22b0dec019d19dbed528cd15
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-04-19 08:50:12 +0200

    Add wednesday

---
+++ wednesday.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 1/ Prenez le tableau suivant et ranger le dans un ordre aléatoire
+ * 
+ * 2/ Puis en suivant le nouvel ordre du tableau, afficher chaque membre dans une grande
+ * sous traction, puis l'effectuer
+ * 
+ * Ex : 
+ * [9, 3, 5, 12]
+ * 
+ * => 9 - 3 - 5 - 12 = -11
+ */
+
+ $values = [6, 78, 3, 5, 26, 14, 1, 9];
